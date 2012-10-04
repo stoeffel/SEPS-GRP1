d16fb2788e8404074f82947978ebe4594f07449f
stoeffel <schtoeffel@gmail.com>
2012-10-04 19:08:07 +0200

    jet

--- app/SpaceFighter/src/ch/zhaw/arsphema/util/Sizes.java
+++ app/SpaceFighter/src/ch/zhaw/arsphema/util/Sizes.java
@@ -3,7 +3,7 @@ package ch.zhaw.arsphema.util;
 public class Sizes {
 
 	public static final float SHIP_WIDTH = 10;
-	public static final float SHIP_HEIGHT = 7.5f;
+	public static final float SHIP_HEIGHT = 4.95f;
 	public static final float DEFAULT_WORLD_WIDTH = 96;
 	public static final float DEFAULT_WORLD_HEIGHT = 84;
 }
