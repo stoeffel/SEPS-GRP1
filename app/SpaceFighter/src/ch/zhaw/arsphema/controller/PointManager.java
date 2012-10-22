6d0c8afd434f68ead0a0765cfd03a23ec3fd729f
aritschcorae <macclonker@gmail.com>
2012-10-22 17:06:24 +0200

    ba

---
+++ app/SpaceFighter/src/ch/zhaw/arsphema/controller/PointManager.java
@@ -0,0 +1,18 @@
+package ch.zhaw.arsphema.controller;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class PointManager {
+	
+	private int points  = 0;
+
+	public void increasePoints(final float timeElapsed, final int points){
+		
+	}
+	
+	public void draw(SpriteBatch batch) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
