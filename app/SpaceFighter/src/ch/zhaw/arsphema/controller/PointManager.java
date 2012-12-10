f1827949a186804b1c42d126d476ceca43db6d5b
stoeffel <schtoeffel@gmail.com>
2012-12-10 17:07:54 +0100

    jaaava doc

--- app/SpaceFighter/src/ch/zhaw/arsphema/controller/PointManager.java
+++ app/SpaceFighter/src/ch/zhaw/arsphema/controller/PointManager.java
@@ -2,10 +2,17 @@ package ch.zhaw.arsphema.controller;
 
 import ch.zhaw.arsphema.util.Sizes;
 import ch.zhaw.arsphema.util.UiStyles;
+
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 
+/**
+ * Punkte Kontroller:
+ * zeigt die Punkte des Spielers an
+ * @author schtoeffel
+ *
+ */
 public class PointManager {
 
     private int enemyPoints = 0;
@@ -14,19 +21,36 @@ public class PointManager {
     private BitmapFont counterfont;
     private Label showMeYourPoints;
 
+    /**
+     * Konstruktor
+     */
     public PointManager() {
         showMeYourPoints = new Label("0", UiStyles.getPointLabelStyle(0));
     }
 
+    /**
+     * erhöht den Punkte zähler um die Punkte die der Gegner wert war.
+     * @param timeElapsed
+     * @param addPoints
+     */
     public void increasePointsOfEnemies(final float timeElapsed, final int addPoints) {
         enemyPoints += addPoints * (timeElapsed * 1.5f);
     }
 
-
+    /**
+     * erhöht die punkte für langes überleben
+     * @param timeElapsed
+     */
     public void increaseTimePoints(final float timeElapsed) {
         timeBonus = (int) (timeElapsed * 13f);
     }
 
+    /**
+     * zeigt die punkte an
+     * @param batch
+     * @param ppuX
+     * @param ppuY
+     */
     public void draw(SpriteBatch batch, float ppuX, float ppuY) {
         totalPoints = enemyPoints + timeBonus;
         showMeYourPoints.setStyle(UiStyles.getPointLabelStyle(ppuY));
@@ -37,6 +61,10 @@ public class PointManager {
         showMeYourPoints.draw(batch, 1);
     }
 
+    /**
+     * getter totalPoints
+     * @return
+     */
     public int getTotalPoints() {
         return totalPoints;
     }
--- app/SpaceFighter/src/ch/zhaw/arsphema/controller/ShotManager.java
+++ app/SpaceFighter/src/ch/zhaw/arsphema/controller/ShotManager.java
@@ -5,10 +5,19 @@ import ch.zhaw.arsphema.model.shot.Shot;
 
 import com.badlogic.gdx.utils.Array;
 
+/**
+ * Schuss Kontroller:
+ * kontrolliert alle schüsse
+ * @author schtoeffel
+ *
+ */
 public class ShotManager {
 
 	private Array<Shot> heroShots, enemyShots, shotsToRemove;
 	
+	/**
+	 * Konstruktor
+	 */
 	public ShotManager()
 	{
 		heroShots = new Array<Shot>();
@@ -16,12 +25,19 @@ public class ShotManager {
 		shotsToRemove = new Array<Shot>();
 	}
 
+	/**
+	 * räumt schüsse auf
+	 */
 	public void cleanUpShots() {
 		removeShots(heroShots);
 		removeShots(enemyShots);
 		shotsToRemove.clear();		
 	}
 	
+	/**
+	 * entfernt schüsse
+	 * @param shotArray
+	 */
 	private void removeShots(final Array<Shot> shotArray) {
 		for(final Shot shot : shotsToRemove)
 		{
@@ -29,6 +45,11 @@ public class ShotManager {
 		}
 	}
 
+	/**
+	 * test ob der held getroffen wurde
+	 * @param hero
+	 * @return
+	 */
 	public boolean heroSuffering(final Hero hero) {
 		for(final Shot shot : enemyShots)
 		{
@@ -41,11 +62,19 @@ public class ShotManager {
 		return false;
 	}
 
+	/**
+	 * fügt hero schüsse hinzu
+	 * @param shots
+	 */
 	public void heroShoots(final Array<Shot> shots) {
 		if(shots  != null )
 			heroShots.addAll(shots);			
 	}
 	
+	/**
+	 * bewegt schüsse
+	 * @param delta
+	 */
 	public void moveShots(final float delta) {
 		for(final Shot shot : enemyShots)
 			shot.move(delta);
@@ -53,27 +82,49 @@ public class ShotManager {
 			shot.move(delta);
 	}
 	
-	
+	/**
+	 * getter für heroschüsse
+	 * @return
+	 */
 	public Array<Shot> getHeroShots() {
 		return heroShots;
 	}
 
+	/**
+	 * setter für heroShots
+	 * @param heroShots
+	 */
 	public void setHeroShots(Array<Shot> heroShots) {
 		this.heroShots = heroShots;
 	}
 
+	/**
+	 * getter für enemyshots
+	 * @return
+	 */
 	public Array<Shot> getEnemyShots() {
 		return enemyShots;
 	}
 
+	/**
+	 * setter für enemyshots
+	 */
 	public void setEnemyShots(Array<Shot> enemyShots) {
 		this.enemyShots = enemyShots;
 	}
 
+	/**
+	 * getter für zulöschende schüsse
+	 * @return
+	 */
 	public Array<Shot> getShotsToRemove() {
 		return shotsToRemove;
 	}
 
+	/**
+	 * setter für zulöschende schüsse
+	 * @param shotsToRemove
+	 */
 	public void setShotsToRemove(Array<Shot> shotsToRemove) {
 		this.shotsToRemove = shotsToRemove;
 	}
