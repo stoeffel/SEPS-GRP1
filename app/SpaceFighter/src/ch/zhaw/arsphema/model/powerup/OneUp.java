046171e9069e9627bb71619e2b4d94de579f21d2
Daniel M. Ferreira <magaldan@students.zhaw.ch>
2012-12-10 17:15:30 +0100

    java doc comments fuer powerups

--- app/SpaceFighter/src/ch/zhaw/arsphema/model/powerup/OneUp.java
+++ app/SpaceFighter/src/ch/zhaw/arsphema/model/powerup/OneUp.java
@@ -4,18 +4,24 @@ import ch.zhaw.arsphema.model.Hero;
 
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
+/**
+ * das lebens powerup
+ */
 public class OneUp extends AbstractPowerUp {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -1360335066820417584L;
 
+	/**
+	 * konstruktor
+	 */
 	public OneUp(float x, float y, float width, float height,
 			TextureRegion texture) {
 		super(x, y, width, height, texture);
 	}
 
+	/**
+	 * addiert ein leben oder stellt ein leben wieder her
+	 */
 	@Override
 	public boolean doSomething(Hero hero) {
 		hero.oneUp();
